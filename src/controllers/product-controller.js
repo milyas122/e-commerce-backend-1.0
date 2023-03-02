f373a7ed0b0e5e2f950bb7833f6d91e774e4f1fa
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-02 13:03:33 +0500

    Product-Controller: get product detail

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -19,14 +19,28 @@ async function getProduct(req, res) {
   const prodId = req.params.id;
   try {
     const product = await Product.findById({ _id: prodId });
+    if (!product)
+      return res
+        .status(201)
+        .json({ product: product, message: "Invalid Product Id" });
     return res.status(201).json({ product: product, message: "Success" });
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ message: "Unable to update product" });
+    return res.status(500).json({ message: "Unable to get product detail" });
   }
 }
 
-// Update Product
+// PUT: products/:id
+// async function updateProduct(req, res) {
+//   const prodId = req.params.id;
+//   try {
+//     const
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).json({ message: "Unable to update product" });
+//   }
+// }
+
 // const { title, price, stock, description, category, images } = req.body;
 
 // Get All Products
