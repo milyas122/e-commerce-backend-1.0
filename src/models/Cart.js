63bbad9c47e29c8c6a35fbc612259ecb795999a4
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-03 16:47:34 +0500

    Cart-Model: Add

---
+++ src/models/Cart.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  productId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Product",
+    require: true,
+  },
+  quantity: Number,
+  total: Number,
+});
+
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = { Cart };
