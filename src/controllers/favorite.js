bb25672631b73b03451d2142aa645aa840828806
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-03 12:33:43 +0500

    Favorite-Controller: remove from favorite

--- src/controllers/favorite.js
+++ src/controllers/favorite.js
@@ -32,5 +32,20 @@ async function addProductToFavorite(req, res) {
 }
 
 // remove product from favorite
+async function removeProductFromFavorite(req, res) {
+  const productId = req.params.id;
+  const userId = req.user.id;
+  try {
+    const favorite = await Favorite.findOneAndDelete({ productId, userId });
+    if (!favorite) {
+      return res.status(400).json({ message: "Product not exist" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Remove from favorite successfully" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error Occurred" });
+  }
+}
 
-module.exports = { addProductToFavorite };
+module.exports = { addProductToFavorite, removeProductFromFavorite };
