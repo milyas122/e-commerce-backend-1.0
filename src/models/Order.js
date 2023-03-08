073a3403fe3ec8665e45c01fca251184e3663624
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-08 14:18:58 +0500

    Models: Add Order model

---
+++ src/models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const orderDetailSchema = new mongoose.Schema({
+  productId: { type: mongoose.Types.ObjectId, ref: "Product", require: true },
+  quantity: { type: Number, require: true },
+  total: { type: Number, require: true },
+});
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  timestamps: true,
+  orderDetail: [
+    {
+      type: orderDetailSchema,
+    },
+  ],
+  total: { type: Number, require: true },
+});
+
+Order = mongoose.Model("Order", orderSchema);
+module.exports = Order;
