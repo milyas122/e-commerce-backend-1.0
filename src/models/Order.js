5008820e460d5e9cb5f3e8daed2b4153828c39ae
Muhammad Ilyas <MuhammadIlyas523221@gmail.com>
2023-03-08 16:01:00 +0500

    Order-Model: Updated

--- src/models/Order.js
+++ src/models/Order.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const orderDetailSchema = new mongoose.Schema({
   productId: { type: mongoose.Types.ObjectId, ref: "Product", require: true },
+  title: String,
+  description: String,
+  category: String,
+  images: [{ type: String }],
   quantity: { type: Number, require: true },
   total: { type: Number, require: true },
 });
